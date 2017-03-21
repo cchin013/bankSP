66a23fbc42e71e5317ab7bb9e6a6853bb108ff20
cchin013 <cchin013@ucr.edu>
2017-03-21 00:42:39 +0000

    Added foundation

---
+++ Menu.h
@@ -0,0 +1,11 @@
+#ifndef MENU_H
+#define MENU_H
+
+class User;
+
+class Interface {
+  public:
+  void Menu();
+};
+
+#endif
--- User.h
+++ User.h
@@ -1,1 +1,22 @@
-
+class User {
+    private:
+    double bal;
+    string name;
+    string accType;
+    vector<User> u;
+    
+    public:
+    User();
+    ~User();
+    void showAccount();
+    void depositMoney();
+    void withdrawMoney();
+    void createAccount();
+    void closeAccount();
+    void changeInfo();
+    int calculateBal();
+    string getName();
+    double getBal();
+    string getAccType();
+    
+};
